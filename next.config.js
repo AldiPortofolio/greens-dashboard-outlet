5edd5bd102ed02745f397a878b0ceb295ee3988f
aldi arviansyah <aldi.arviansyah@ottodigital.id>
2022-06-20 04:01:34 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const withImages = require('next-images');
+/** @type {import('next').NextConfig} */
+const withImages = require('next-images');
 
-// const nextConfig = withImages({
-//   reactStrictMode: true,
-//   // swcMinify: true,
-//   images: {
-//     disableStaticImages: true,
-//   },
-// });
-
-// module.exports = nextConfig;
-const withReactSvg = require('next-react-svg');
-const path = require('path');
-
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, 'app/assets'),
-  webpack(config, options) {
-    return config;
-  },
+const nextConfig = withImages({
+  reactStrictMode: true,
 });
+
+module.exports = nextConfig;
